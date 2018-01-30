23d295261a7302cb55f0b60f26afa490e6f83dfa
erick <erickarmandogarcia@hotmail.com>
2018-01-30 17:24:13 -0500

    Add css-hot-loader to webpack, modify scripts to run the app, add production server in express

--- webpack.config.js
+++ webpack.config.js
@@ -31,10 +31,10 @@ let config = {
 			},
 			{
 				test: /\.scss$/,// files ending with .scss,
-				use: ExtractTextWebpackPlugin.extract({ //call our plugin with extract method
+				use: ['css-hot-loader'].concat(ExtractTextWebpackPlugin.extract({ //call our plugin with extract method
 					use: ['css-loader', 'sass-loader'], //use these loaders
 					fallback: 'style-loader' //fallback for any CSS not extractrd
-				})//end extract
+				}))//end extract
 			},
 			{
 				test: /\.jsx$/, //all files ending with .jsx
